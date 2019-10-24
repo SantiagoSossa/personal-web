616a3ac5ae5e85a175e116a3e82b9496e3379a96
santiagosossa <santiagosossa95@gmail.com>
2019-10-23 23:04:41 -0500

    [feat] modal
    added modal to see more info about each proyect, some CSS changes in the proyect component

--- src/components/Proyects/Proyect/Proyect.js
+++ src/components/Proyects/Proyect/Proyect.js
@@ -1,43 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Proyect.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
-import { faLaptopCode } from '@fortawesome/free-solid-svg-icons'
+import { faLaptopCode, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 
 import Fade from 'react-reveal/Fade';
 
-const Proyect = (props) => (
-    <Fade duration={1500} bottom>
-    <div className={classes.Proyect}>
-        <div className={classes.Card}>
-            <div className={[classes.Face,classes.Face1].join(" ")}>
-                <div className={classes.Content}>
-                    <img src={props.image} alt="Test"/>
-                    <h3>{props.title}</h3>
-                </div>
-            </div>
-            <div className={[classes.Face,classes.Face2].join(" ")}>
-                <div className={classes.Content}>
-                    <p>{props.description}</p>
-                    <div className={classes.Icons}>
-                        <a href={props.githubLink} target="_blank">
-                            <FontAwesomeIcon 
-                                title="View Code" 
-                                className={classes.Icon} 
-                                icon={faGithubSquare} />
-                            </a>
-                        <a href={props.demoLink} target="_blank">
-                            <FontAwesomeIcon 
-                                title="View Demo" 
-                                className={classes.Icon} 
-                                icon={faLaptopCode} />
-                        </a>
+export default class Proyect extends Component {
+    render() {
+        return(
+            <Fade duration={1500} bottom>
+                <div className={classes.Proyect}>
+                    <div className={classes.Card}>
+                        <div className={[classes.Face,classes.Face1].join(" ")}>
+                            <div className={classes.Content}>
+                                <img src={this.props.image} alt="Test"/>
+                                <h3>{this.props.title}</h3>
+                            </div>
+                        </div>
+                        <div className={[classes.Face,classes.Face2].join(" ")}>
+                            <div className={classes.Content}>
+                                <p>{this.props.description}</p>
+                                <div className={classes.Icons}>
+                                    <a href={this.props.githubLink} target="_blank">
+                                        <FontAwesomeIcon 
+                                            title="View Code" 
+                                            className={classes.Icon} 
+                                            icon={faGithubSquare} />
+                                        </a>
+                                    <a href={this.props.demoLink} target="_blank">
+                                        <FontAwesomeIcon 
+                                            title="View Demo" 
+                                            className={classes.Icon} 
+                                            icon={faLaptopCode} />
+                                    </a>
+                                    <FontAwesomeIcon 
+                                        onClick={this.props.show}
+                                        title="More Info" 
+                                        className={classes.Icon} 
+                                        icon={faInfoCircle} />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </div>
-    </Fade>
-);
-
-export default Proyect;
+            </Fade>
+        )
+    }
+}
--- src/components/Proyects/Proyects.js
+++ src/components/Proyects/Proyects.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Modal from '../Modal/Modal'; 
 import Proyect from './Proyect/Proyect';
 import classes from './Proyects.module.css';
 import CollegeRecipes from '../../assets/images/CollegeRecipes.png';
@@ -6,11 +7,30 @@ import Conexion from '../../assets/images/Conexion.png';
 import Portfolio from '../../assets/images/Portfolio.png';
 
 class Proyects extends Component {
-    render() {
-    const style = {
-        color: this.props.colors.text,
-        background: this.props.colors.bg3
+    state = {
+        moreInfo: false
     }
+
+    handleModalOpen = () => {
+        this.setState({moreInfo: true});
+    }
+
+    handleModalClose = () => {
+        this.setState({moreInfo: false});
+    }
+
+    render() {
+        const style = {
+            color: this.props.colors.text,
+            background: this.props.colors.bg3
+        }
+        let modal = null;
+        if(this.state.moreInfo){
+            modal = <Modal 
+                show={this.state.moreInfo}
+                modalClosed={this.handleModalClose}>
+            </Modal>;
+        }
         return(
             <div id="proyects" className={classes.Proyects} style={style}>
                 <h3>Proyects</h3>
@@ -21,20 +41,24 @@ class Proyects extends Component {
                         title="College Recipes" 
                         description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni minus consectetur nihil eveniet delectus obcaecati unde quidem animi!" 
                         githubLink="https://github.com/SantiagoSossa/college-recipes" 
-                        demoLink="https://college-recipes.firebaseapp.com/"/>
+                        demoLink="https://college-recipes.firebaseapp.com/"
+                        show={this.handleModalOpen}/>
                     <Proyect 
                         image={Conexion}
                         title="Conexion EyS" 
                         description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni minus consectetur nihil eveniet delectus obcaecati unde quidem animi!" 
                         githubLink="https://github.com/SantiagoSossa/conexion-webpage" 
-                        demoLink="http://conexioneventosyservicios.com/"/>
+                        demoLink="http://conexioneventosyservicios.com/"
+                        show={this.handleModalOpen}/>
                     <Proyect 
                         image={Portfolio}    
                         title="This Portfolio" 
                         description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni minus consectetur nihil eveniet delectus obcaecati unde quidem animi!" 
                         githubLink="https://github.com/SantiagoSossa/personal-web" 
-                        demoLink="https://www.santiagosossa.com/"/>
+                        demoLink="https://www.santiagosossa.com/"
+                        show={this.handleModalOpen}/>
                 </div>
+                {modal}
             </div>
         )
     }
